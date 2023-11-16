37636a674f789a4796f099970661b6df3a495f9b
Laura <milanbartky@gmail.com>
2023-11-17 00:22:39 +0100

    bigger web-safe list

--- known_fonts.js
+++ known_fonts.js
@@ -8,13 +8,14 @@ export default [
     "monospace",
     "cursive",
 
-    "Arial",
-    "Verdana",
-    "Tahoma",
-    "Trebuchet MS",
-    "Times New Roman",
-    "Georgia",
-    "Garamond",
-    "Courier New",
-    "Brush Script MT"
+    // Commonly considered web-safe fonts
+    "Andale Mono", "Arial", "Arial Black", "Arial Narrow", "Arial Rounded MT Bold",
+    "Avant Garde", "Baskerville", "Big Caslon", "Bodoni MT", "Book Antiqua",
+    "Brush Script MT", "Calibri", "Calisto MT", "Cambria", "Candara", "Century Gothic",
+    "Consolas", "Copperplate", "Courier New", "Didot", "Franklin Gothic Medium",
+    "Futura", "Garamond", "Geneva", "Georgia", "Gill Sans", "Goudy Old Style",
+    "Helvetica", "Hoefler Text", "Impact", "Lucida Bright", "Lucida Console",
+    "Lucida Grande", "Lucida Sans Typewriter", "Monaco", "Optima", "Palatino",
+    "Papyrus", "Perpetua", "Rockwell", "Rockwell Extra Bold", "Segoe UI", "Tahoma",
+    "Times New Roman", "Trebuchet MS", "Verdana"
 ];
